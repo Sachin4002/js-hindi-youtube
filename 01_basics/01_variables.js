f7138719565cc710f1c4be16c9fd58a91f5d689a
Sachin4002 <143272001+Sachin4002@users.noreply.github.com>
2023-08-26 15:45:28 +0000

    Variables discussed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =123654
+let accountEmail="sachin@gmail.com"
+var accountPassword= "12345"
+
+accountCity="Jaipur"
+accountEmail="dbdb@gmail.com"
+accountPassword='456'
+ var accountState;
+
+console.log([accountEmail,accountId,accountCity,accountPassword,accountState]);
+
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState]);
+
+
