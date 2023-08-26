a6bc75f513f35753a0d30265dbd702917e5477a7
Sachin4002 <143272001+Sachin4002@users.noreply.github.com>
2023-08-26 16:15:51 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+let name="Sachin"
+let age= 35
+let isloggedIn=false
+let state = null
+
+// number ===> 2 to power of 53
+//bigInt
+//string==> ""
+//boolean ==> true or false
+
+//null==> object-- standalone value
+
+//undefined==> not defined any value
+
+// symbol=> will be used in react for uniqueness of components
+
+
+//object
+
+console.log(typeof isloggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
+
