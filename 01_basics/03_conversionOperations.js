8508ff3c8609db50c2995c72ab555d15d8dd516a
Sachin4002 <143272001+Sachin4002@users.noreply.github.com>
2023-08-26 19:48:51 +0000

    comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,3 +14,18 @@ console.log(typeof(value2));
 //1=> true;  0=> false
 //"" ==> false
 //"Sachin"==> true
+
+/******OPertaions******* */ 
+
+let value=9;
+let negvalue=-value;
+console.log(negvalue);
+let str1 ='Sachin'
+let str2=' Narwade'
+
+console.log(str1+str2);
+
+console.log("1"+2); //--->12
+
+console.log(1+"2");  //--->12
+console.log("1"-"2"); 
