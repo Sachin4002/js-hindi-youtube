c7a5fa925aff9a37bdf8bcf6815482d869ceed4d
Sachin4002 <143272001+Sachin4002@users.noreply.github.com>
2023-08-26 19:22:00 +0000

    Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let score="33";
+let num= 123
+console.log(typeof(score));
+console.log(typeof(score));
+
+var value1=Number(score)
+var value2=String(num)
+
+console.log("Changed ones");
+console.log(typeof(value1));
+console.log(typeof(value2));
+
+
+//1=> true;  0=> false
+//"" ==> false
+//"Sachin"==> true
